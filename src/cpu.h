b6409802eb1d3518df1eda657b2e611a778334ff
ang9009 <ngalvin2004@gmail.com>
2025-03-23 01:36:01 +0000

    Refactor registers to unions

--- src/cpu.h
+++ src/cpu.h
@@ -1,24 +1,66 @@
 #include <stdint.h>
 
-typedef struct {
-    uint8_t A; // Accumulator register
-    uint8_t B;
-    uint8_t C;
-    uint8_t D;
-    uint8_t E;
-    uint8_t H;
-    uint8_t L;
-} registers_t;
+/**
+ * CPU registers
+ */
+
+// Flags struct for the AF register
+typedef union {
+    struct {
+        uint8_t : 4; // Upper 4 bits ignored
+        uint8_t Z: 1;
+        uint8_t N: 1;
+        uint8_t H: 1;
+        uint8_t C: 1;
+    };
+    uint8_t reg;
+} flags_reg_t;
+
+typedef union {
+    struct {
+        uint8_t A;
+        flags_reg_t F;
+    };
+    uint16_t reg;
+} af_reg_t;
+
+typedef union {
+    struct {
+        uint8_t B;
+        uint8_t C;
+    };
+    uint16_t reg;
+} bc_reg_t;
+
+typedef union {
+    struct {
+        uint8_t D;
+        uint8_t E;
+    };
+    uint16_t reg;
+} de_reg_t;
+
+typedef union {
+    struct {
+        uint8_t H;
+        uint8_t L;
+    };
+    uint16_t reg;
+} hl_reg_t;
+
 
 typedef struct {
-    uint8_t Z;
-    uint8_t N;
-    uint8_t H;
-    uint8_t C;
-} flags_t;
-
-typedef struct {
-    registers_t registers;
+    af_reg_t AF;
+    bc_reg_t BC;
+    de_reg_t DE;
+    hl_reg_t HL;
     uint16_t SP;
     uint16_t PC;
+} cpu_regs_t;
+
+/**
+ * CPU
+ */
+typedef struct {
+    cpu_regs_t regs;
 } cpu_t;
