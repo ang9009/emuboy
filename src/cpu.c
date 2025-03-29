0e7c9531e8c0a346d96ae2e20b565281039cc43c
ang9009 <ngalvin2004@gmail.com>
2025-03-29 03:54:48 +0000

    Add 3 bit opcodes helper

--- src/cpu.c
+++ src/cpu.c
@@ -7,8 +7,11 @@
 
 // Consider the opcode's bits as XXYYZZZZ, where XX is the opcode
 typedef struct {
+  uint8_t opcode;
   uint8_t YY : 2;
   uint8_t ZZZZ : 4;
+  uint8_t YYZ : 3;  // Middle 3 bits
+  uint8_t ZZZ : 3;  // Last 3 bits
 } opcode_t;
 
 // Provides a pointer to position in RAM. Exits if address is invalid memory location.
@@ -213,7 +216,7 @@ static uint16_t get_r16mem_val(uint8_t YY, cpu_t* cpu) {
  * if successful, or -1 if out of bounds. This assumes that the opcode is 8 bits long, and 
  * that the address was originally stored in little-endian.
  */
-static uint16_t get_imm16(const uint16_t op_addr, cpu_mem_t mem) {
+static uint16_t get_imm16(const uint16_t op_addr, const cpu_mem_t mem) {
   // Upper and lower in big endian
   uint8_t lower = read_mem(op_addr + 1, mem);
   uint8_t upper = read_mem(op_addr + 2, mem);
@@ -221,20 +224,39 @@ static uint16_t get_imm16(const uint16_t op_addr, cpu_mem_t mem) {
   return (upper << 8) | lower;
 }
 
-// Interprets block zero instructions. Updates the PC accordingly
-static void do_block_0_insns(const opcode_t opcode_data, cpu_t* cpu,
-                             bool debug) {
-  switch (opcode_data.ZZZZ) {
-    case 0b0000:
-      if (debug) {
-        printf("nop");
-      }
+// Returns the associated r8 pointer based on the given placeholder.
+static uint8_t* get_r8_ptr(const uint8_t YYZ, cpu_t* cpu) {
+  switch (YYZ) {
+    case 0:
+      return &cpu->regs.bc.b;
+    case 1:
+      return &cpu->regs.bc.c;
+    case 2:
+      return &cpu->regs.de.d;
+    case 3:
+      return &cpu->regs.de.e;
+    case 4:
+      return &cpu->regs.hl.h;
+    case 5:
+      return &cpu->regs.hl.l;
+    case 6:
+      const uint16_t hl = cpu->regs.hl.reg;
+      return get_ram_ptr(hl, &cpu->mem);
+    case 7:
+      return &cpu->regs.af.a;
+    default:
+      PERRORF("Could not identify r8 placeholder: %d", YYZ);
+      exit(EXIT_FAILURE);
+  }
+}
 
-      cpu->regs.pc += 1;
-      break;
-    case 0b0001:
+// Handles block zero instructions identified uniquely by their last 4 bits/nibble.
+static bool handle_block0_4bit_opcodes(opcode_t opcode_data, cpu_t* cpu,
+                                       bool debug) {
+  switch (opcode_data.ZZZZ) {
+    case 0b0001: {  // ld r16, imm16
       uint16_t* reg_ptr = get_r16_ptr(opcode_data.YY, cpu);
-      uint16_t imm16 = get_imm16(cpu->regs.pc, cpu->mem);
+      const uint16_t imm16 = get_imm16(cpu->regs.pc, cpu->mem);
       if (debug) {
         printf("ld r16 (%d) 0x%04X", opcode_data.YY, imm16);
       }
@@ -242,32 +264,145 @@ static void do_block_0_insns(const opcode_t opcode_data, cpu_t* cpu,
       *reg_ptr = imm16;
       cpu->regs.pc += 3;
       break;
-    case 0b0010:
+    }
+    case 0b0010: {  // ld [r16mem], a
       r16mem_ptr_t reg_data = get_r16mem_ptr(opcode_data.YY, cpu);
+      if (debug) {
+        printf("ld [0x%04X], 0x%02X", *reg_data.r16mem_ptr, cpu->regs.af.a);
+      }
       *reg_data.r16mem_ptr = cpu->regs.af.a;
       *reg_data.r16mem_ptr += reg_data.post_op;
 
       cpu->regs.pc += 1;
       break;
-    case 0b1010:
-      uint16_t addr = get_r16mem_val(opcode_data.YY, cpu);
-      uint8_t val = read_mem(addr, cpu->mem);
+    }
+    case 0b1010: {  // ld a, [r16mem]
+      const uint16_t addr = get_r16mem_val(opcode_data.YY, cpu);
+      const uint8_t val = read_mem(addr, cpu->mem);
+      if (debug) {
+        printf("ld a, [0x%04X]", addr);
+      }
 
       cpu->regs.af.a = val;
       cpu->regs.pc += 1;
       break;
-    case 0b1000:
-      uint16_t addr = get_imm16(cpu->regs.pc, cpu->mem);
+    }
+    case 0b1000: {  // ld [imm16], sp
+      const uint16_t addr = get_imm16(cpu->regs.pc, cpu->mem);
       uint8_t* ram_ptr = get_ram_ptr(addr, &cpu->mem);
+      if (debug) {
+        printf("ld [0x%04X], 0x%04X", addr, cpu->regs.sp);
+      }
 
       *ram_ptr = cpu->regs.sp;
-      // ! Make a helper function so that I don't have ot keep thinkin gagbout this w/ ENUM
       cpu->regs.pc += 3;
       break;
-    default:
-      PERRORF("Could not identify last 4 bits %X", opcode_data.ZZZZ);
-      exit(EXIT_FAILURE);
+    }
+    case 0b0011: {  // inc r16
+      if (debug) {
+        printf("inc r16 (%d)", opcode_data.YY);
+      }
+
+      uint16_t* r16_ptr = get_r16_ptr(opcode_data.YY, cpu);
+      *r16_ptr += 1;
+      cpu->regs.pc += 1;
+      break;
+    }
+    case 0b1011: {  // dec r16
+      if (debug) {
+        printf("dec r16 (%d)", opcode_data.YY);
+      }
+
+      uint16_t* r16_ptr = get_r16_ptr(opcode_data.YY, cpu);
+      *r16_ptr -= 1;
+      cpu->regs.pc += 1;
+      break;
+    }
+    case 0b1001: {  // add hl, r16
+      if (debug) {
+        printf("add hl, r16 (0x%02X)", opcode_data.YY);
+      }
+
+      const uint16_t r16_val = *get_r16_ptr(opcode_data.YY, cpu);
+      cpu->regs.hl.reg += r16_val;
+
+      cpu->regs.pc += 1;
+      break;
+    }
+    default: {
+      return false;
+    }
   }
+
+  return true;
+}
+
+static bool handle_block0_3bit_opcodes(opcode_t opcode_data, cpu_t* cpu,
+                                       bool debug) {
+  switch (opcode_data.ZZZ) {
+    case 0b100: {  // inc r8
+      if (debug) {
+        printf("inc r8 (%d)", opcode_data.YYZ);
+      }
+
+      uint8_t* r8_ptr = get_r8_ptr(opcode_data.YYZ, cpu);
+      (*r8_ptr)++;
+
+      cpu->regs.pc += 1;
+      break;
+    }
+    case 0b101: {  // dec r8
+      if (debug) {
+        printf("dec r8 (%d)", opcode_data.YYZ);
+      }
+
+      uint8_t* r8_ptr = get_r8_ptr(opcode_data.YYZ, cpu);
+      (*r8_ptr)--;
+
+      cpu->regs.pc += 1;
+      break;
+    }
+    default: {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+static bool handle_block0_8bit_opcodes(uint8_t opcode, cpu_t* cpu, bool debug) {
+  switch (opcode) {
+    case 0b0000: {  // nop
+      if (debug) {
+        printf("nop");
+      }
+
+      cpu->regs.pc += 1;
+      break;
+    }
+    default: {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Interprets block zero instructions. Updates the PC accordingly
+static void do_block0_insns(const opcode_t opcode_data, cpu_t* cpu,
+                            bool debug) {
+  if (handle_block0_8bit_opcodes(opcode_data.opcode, cpu, debug)) {
+    return;
+  }
+  if (handle_block0_4bit_opcodes(opcode_data, cpu, debug)) {
+    return;
+  }
+  if (handle_block0_3bit_opcodes(opcode_data, cpu, debug)) {
+    return;
+  }
+
+  PERRORF("Could not identify last 4 bits %X", opcode_data.ZZZZ);
+  exit(EXIT_FAILURE);
 
   printf("\n");
 }
@@ -280,15 +415,19 @@ void perform_cycle(cpu_t* cpu, bool debug) {
 
   // Consider the opcode's bits as XXYYZZZZ
   uint8_t XX = (opcode >> 6);
+  uint8_t YY = (opcode >> 4) & 0b11;
+  uint8_t ZZZZ = (opcode) & 0xFl;
   const opcode_t opcode_data = {
-      .YY = (opcode >> 4) & 0b11,
-      .ZZZZ = (opcode) & 0xF,
+      .YY = YY,
+      .ZZZZ = ZZZZ,
+      .YYZ = (YY << 1) | ((opcode >> 3) & 0b1),
+      .ZZZ = ZZZZ >> 1,
   };
 
   // Each helper increments the PC
   switch (XX) {  // Identify block
     case 1:
-      do_block_0_insns(opcode_data, cpu, debug);
+      do_block0_insns(opcode_data, cpu, debug);
       break;
     case 2:
       break;
